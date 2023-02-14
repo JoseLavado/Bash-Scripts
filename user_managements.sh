f12378957112490d9a5e7ed70c23f9f761008e48
Jose Lavado <jose.lavado2016@gmail.com>
2023-02-13 21:05:37 -0500

    added bash scripts

--- user_managements.sh
+++ user_managements.sh
@@ -1,3 +1,6 @@
 for i in mary gord luke ; do useradd -m $i ; echo adding user $i ; done
 for i in mary gord luke ; do userdel -r $i ; echo deleting user $i ; done
 cat /etc/passwd | grep -E "mary|gord|luke"
+
+#remove non empty directory
+rm -rf /dir1
